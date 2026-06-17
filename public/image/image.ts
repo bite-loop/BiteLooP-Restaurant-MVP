0d577481577382edee0b10e9e19a8653b3c0eaf0
IgYaHiko <subhrokolay2@gmail.com>
2026-06-17 20:08:21 +0530

    [SETUP]-[02]:setup-project-shadcn

---
+++ public/image/image.ts
@@ -0,0 +1,50 @@
+import BURGER from "./burger.png"
+import SLURPY from "./slurpy.png"
+import SLICE from './slice.png'
+import LEG from './leg.png'
+import PASTRY from './pastry.png'
+import FRIES from './fries.png'
+import PIN from './pin.png'
+import OUTLATE from './outlate.png'
+import PACK from './pack.png'
+import RESTAURANTSONE from './restrunt/res1.jpg'
+import RESTAURANTSTWO from './restrunt/res2.jpg'
+import RESTAURANTSTHREE from './restrunt/res3.jpg'
+import RESTAURANTSFOUR from './restrunt/res4.jpg'
+import RESTAURANTSFIVE from './restrunt/res5.jpg'
+import RESTAURANTSSIX from './restrunt/res6.jpg'
+import DINE from './dine.png'
+import YUM from './yum.png'
+import ARROW from './arrow.png'
+import PHONE from './phone.png'
+import RECOM from './recom.png'
+import SMILEY from './smiley.png'
+export const FOOD = {
+     BURGER,
+     SLURPY,
+     SLICE,
+     LEG,
+     PASTRY,
+     FRIES
+}
+
+export const VECTORS = {
+     PIN,
+     OUTLATE,
+     PACK,
+     ARROW,
+     DINE,
+     YUM,
+     SMILEY,
+     RECOM,
+     PHONE
+}
+export const RESTAURANTSIMG = {
+    RESTAURANTSONE,
+    RESTAURANTSTWO,
+    RESTAURANTSTHREE,
+    RESTAURANTSFOUR,
+    RESTAURANTSFIVE,
+    RESTAURANTSSIX
+}
+
