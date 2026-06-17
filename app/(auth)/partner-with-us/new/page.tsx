cea7f3ea515192d80fdf71d66d6cd9cf9aad360d
IgYaHiko <subhrokolay2@gmail.com>
2026-06-18 00:02:50 +0530

    [PATCH]-[03]:adding-initial-work

---
+++ app/(auth)/partner-with-us/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RegisterNew = () => {
+  return (
+    <div>RegisterNew</div>
+  )
+}
+
+export default RegisterNew
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
+'use client'
+import SmoothScrolling from '@/components/scroll/smooth-scrolling'
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
 
 const page = () => {
+  const route = useRouter()
+  useEffect(() => {
+    setTimeout(() => {
+      route.push("/onboarding")
+    }, 0);
+  },[])
   return (
     <div className=''>
-      <Button>
-        click
-      </Button>
+     <SmoothScrolling/>
     </div>
   )
 }
